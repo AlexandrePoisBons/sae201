c956d41063cc4006cddd07287754e99ea034e7b4
XoleKo <xolekoiut76@gmail.com>
2022-06-10 09:28:14 +0200

    .

--- TestCuve.java
+++ TestCuve.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.io.FileOutputStream;
 
 // EN cas d'encodage en utf-8 import cette classe :
-//  import java.io.OutputStreamWriter;
+// import java.io.OutputStreamWriter;
 
 public class TestCuve
 {
@@ -12,6 +12,7 @@ public class TestCuve
 		/*-----------------------------*/
 		/*			Variables 		   */
 		/*-----------------------------*/
+		Cuve cuve1 = new Cuve(1000, 50, 50, "Haut");
 
 		System.out.print("Combien de cuves ? ");
 
@@ -30,11 +31,9 @@ public class TestCuve
         // PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream("sortie.txt"), "UTF8" ));
 		try
 		{
-			PrintWriter pw = new PrintWriter( new FileOutputStream("sortie.txt") );
+			PrintWriter pw = new PrintWriter( new FileOutputStream("resultat.txt") );
 
-			pw.println ( "Pierre\tDubois\n" );
-			pw.println ( "Régis\tLoisel\n" );
-			pw.println ( "René\tGoscinny" );
+			pw.println ( cuve1.toString() );
 
 			pw.close();
 		}
