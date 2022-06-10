3e18c05c61aec6149b198a1cd6211a87a945c00e
XoleKo <xolekoiut76@gmail.com>
2022-06-10 13:29:53 +0200

    Maj Controleur

--- IHM/Controleur.java
+++ IHM/Controleur.java
@@ -1,4 +1,16 @@
 public class Controleur
 {
-	
+	private Metier   metier;
+	private FrameGUI ihm;
+
+	public Controleur()
+	{
+		this.metier = new Metier();
+		this.ihm    = new FrameGUI();
+	}
+
+	public static void main (String[] args)
+	{
+		new Controleur();
+	}
 }
