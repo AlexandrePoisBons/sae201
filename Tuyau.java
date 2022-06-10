c6ffd68d98ab517a279acab7f4aadd0f8ee0dfa7
VorTekS1 <92797069+VorTekS1@users.noreply.github.com>
2022-06-10 10:24:29 +0200

    Modif toyau

--- Tuyau.java
+++ Tuyau.java
@@ -1,4 +1,13 @@
 public class Tuyau
 {
+    Cuve cuve1;
+    Cuve cuve2;
+    int section;
+    
     public Tuyau(Cuve cuve1, Cuve cuve2, int section)
+    {
+        this.cuve1 = cuve1;
+        this.cuve2 = cuve2;
+        this.section = section;
+    }
 }
