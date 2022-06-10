94f8c98de858159af7d7db2014da3ce822395c4e
VorTekS1 <92797069+VorTekS1@users.noreply.github.com>
2022-06-10 10:27:10 +0200

    factory toyau

--- Tuyau.java
+++ Tuyau.java
@@ -3,11 +3,17 @@ public class Tuyau
     Cuve cuve1;
     Cuve cuve2;
     int section;
-    
-    public Tuyau(Cuve cuve1, Cuve cuve2, int section)
+
+    private Tuyau(Cuve cuve1, Cuve cuve2, int section)
     {
         this.cuve1 = cuve1;
         this.cuve2 = cuve2;
         this.section = section;
+    }
+
+    public Tuyau creerTuyau(Cuve cuve1, Cuve cuve2, int section)
+    {
+        if (section < 2 || section > 10) return null;
+        return new Tuyau(cuve1, cuve2, section);
     }
 }
