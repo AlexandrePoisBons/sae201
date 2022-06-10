5121c4eee4b1c1368733deed09ecb883bd128c71
VorTekS1 <92797069+VorTekS1@users.noreply.github.com>
2022-06-10 10:27:36 +0200

    Ajout tuyau

--- Tuyau.java
+++ Tuyau.java
@@ -11,9 +11,9 @@ public class Tuyau
         this.section = section;
     }
 
-    public Tuyau creerTuyau(Cuve cuve1, Cuve cuve2, int section)
+    public static Tuyau creerTuyau(Cuve cuve1, Cuve cuve2, int section)
     {
         if (section < 2 || section > 10) return null;
         return new Tuyau(cuve1, cuve2, section);
-    }
+    }   
 }
