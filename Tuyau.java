f36ccbf1071657cac4370e9deeb30e1cef6102d0
Alexandre Pois--Bons <alexandrepoisbons@icloud.com>
2022-06-10 11:17:04 +0200

    Update Tuyau.java
    Rename attribute and add new methods

--- Tuyau.java
+++ Tuyau.java
@@ -1,30 +1,38 @@
 public class Tuyau
 {
-    Cuve cuve1;
-    Cuve cuve2;
-    int section;
+    Cuve cuveOrig;
+    Cuve cuveDest;
+    int  section;
 
-    private Tuyau(Cuve cuve1, Cuve cuve2, int section)
+    private Tuyau(Cuve cuveOrig, Cuve cuveDest, int section)
     {
-        this.cuve1 = cuve1;
-        this.cuve2 = cuve2;
-        this.section = section;
+        this.cuveOrig        = cuveOrig;
+        this.cuveDestination = cuveDest;
+        this.section         = section;
     }
 
-    public static Tuyau creerTuyau(Cuve cuve1, Cuve cuve2, int section)
+    public static Tuyau creerTuyau(Cuve cuveOrig, Cuve cuveDest, int section)
     {
-        if (section < 2 || section > 10) return null;
-        if (cuve1 == null || cuve2 == null) return null;
-        return new Tuyau(cuve1, cuve2, section);
+        if (section < 2 || section > 10) 
+            return null;
+        
+        return new Tuyau (null, null, section);
     }
 
-    public Cuve getCuve1()  {return this.cuve1;  }
-    public Cuve getCuve2()  {return this.cuve2;  }
-    public int  getSection(){return this.section;}
-
-    
-    public String toString()
+    public boolean setLien( Cuve cuvePara )
     {
-        return this.cuve1.idCuve + "----- " + this.section + " ------" + this.cuve2.idCuve;
+        this.cuveOrig = this;
+        this.cuveDest = cuvePara;
     }
+
+    // Getters
+    public Cuve getCuveOrig() { return this.cuveOrig; }
+    public Cuve getCuveDest() { return this.cuveDest; }
+    public int  getSection () { return this.section;  }
+
+     public String toString()
+    {
+        return this.cuveOrig.idCuve + "----- " + this.section + " ------" + this.cuveDest.idCuve;
+    }
+
 }
