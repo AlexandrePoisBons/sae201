28c022f8df9743b357624a519699bf7a28fbb273
VorTekS1 <92797069+VorTekS1@users.noreply.github.com>
2022-06-10 15:52:08 +0200

    changmeents matrice

--- metier/Metier.java
+++ metier/Metier.java
@@ -31,7 +31,10 @@ public class Metier
             for (Tuyau t: lstTuyaux)
             {
                 if (t.getCuveOrig() == c || t.getCuveDest() == c)
-                matrice[ (int) (  t.getCuveDest().getId() - 'A'  ) ] [ (int) (t.getCuveOrig().getId() - 'A')] = t.getSection();
+                {
+                    matrice[ (int) (  t.getCuveDest().getId() - 'A'  ) ] [ (int) (  t.getCuveOrig().getId() - 'A'  ) ] = t.getSection();
+                    matrice[ (int) (  t.getCuveOrig().getId() - 'A'  ) ] [ (int) (  t.getCuveDest().getId() - 'A'  ) ] = t.getSection();
+                }
             }
         }
         String sRet ="";
@@ -48,6 +51,7 @@ public class Metier
 
     public static void main(String[] args)
     {
+        //------------------------- Creation de cuves -------------------------//
         Cuve c1 = Cuve.creerCuve(500, 50, 50, "Haut");
         System.out.println(c1);
 
@@ -57,23 +61,32 @@ public class Metier
         Cuve c3 = Cuve.creerCuve(500, 60, 20, "Droite");
         System.out.println(c3);
 
+        //------------------------- Creation de tuyaux -------------------------//
         Tuyau t1c1c2 = Tuyau.creerTuyau(c1, c2, 5);
         t1c1c2.setLien(c1,c2);
         System.out.println(t1c1c2);
 
-        Tuyau t1c1c3 = Tuyau.creerTuyau(c1, c3, 8);
-        t1c1c3.setLien(c1,c3);
-        System.out.println(t1c1c3);
+        Tuyau t2c1c3 = Tuyau.creerTuyau(c1, c3, 8);
+        t2c1c3.setLien(c1,c3);
+        System.out.println(t2c1c3);
 
+        Tuyau t3c2c3 = Tuyau.creerTuyau(c2, c3, 3);
+        t3c2c3.setLien(c2,c3);
+        System.out.println(t3c2c3);
 
+        //------------------------- Creation de matrice -------------------------//
         ArrayList<Cuve>  lstTempC  = new ArrayList<Cuve>(); 
         ArrayList<Tuyau> lstTempT  = new ArrayList<Tuyau>(); 
+        
+        // Ajout cuves dans liste de cuves //
         lstTempC.add(c1);
         lstTempC.add(c2);
         lstTempC.add(c3);
 
+        // Ajout tuyaux dans liste de tuyaux //
         lstTempT.add(t1c1c2);
-        lstTempT.add(t1c1c3);
+        lstTempT.add(t2c1c3);
+        lstTempT.add(t3c2c3);
 
         System.out.println(creerMatrice(lstTempC, lstTempT, 3));
     }
