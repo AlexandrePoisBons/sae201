392be77cdec440507e61cda4ddf6a0c6fafd9f09
VorTekS1 <92797069+VorTekS1@users.noreply.github.com>
2022-06-10 10:53:36 +0200

    ajout main de test

--- Metier.java
+++ Metier.java
@@ -4,4 +4,16 @@ public class Metier
     {
         // blablabla
     }
+
+    public static void main(String[] args)
+    {
+        Cuve c1 = Cuve.creerCuve(500, 50, 50, "Haut");
+        System.out.println(c1);
+
+        Cuve c2 = Cuve.creerCuve(300, 10, 10, "Bas");
+        System.out.println(c2);
+
+        Tuyau t1c1c2 = Tuyau.creerTuyau(c1, c2, 5);
+        System.out.println(t1c1c2);
+    }
 }
