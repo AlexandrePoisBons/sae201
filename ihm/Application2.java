560ace18f40e1642cb5957637054bd3a5b500ea6
XoleKo <xolekoiut76@gmail.com>
2022-06-15 08:39:31 +0200

    Application2 & FrameSelectFichier
    Placement corrrect des setSize / setResizable / setVisible

--- ihm/Application2.java
+++ ihm/Application2.java
@@ -9,12 +9,14 @@ public class Application2 extends JFrame
     public Application2()
     {
         this.panel = new panelApplication2(this);
+
         this.setTitle("Application 2");
-        //this.setSize(300, 100);
-        this.setResizable(false);
-        this.setSize(300, 300);
-        this.setVisible(true);
+        this.setSize (300, 300);
+        
         this.add(this.panel);
+
+        this.setResizable(false);
+        this.setVisible(true);
     } 
 
     public static void main(String[] args)
--- ihm/FrameSelectFichier.java
+++ ihm/FrameSelectFichier.java
@@ -10,12 +10,15 @@ public class FrameSelectFichier extends JFrame
     private ControleurCuves    ctrl;
     public FrameSelectFichier(ControleurCuves ctrl)
     {
-        this.ctrl = ctrl;
+        this.ctrl  = ctrl;
         this.panel = new panelSelectFichier(this, this.ctrl);
+
         this.setTitle("Choix du fichier");
-        this.setResizable(false);
-        this.setSize(300, 300);
-        this.setVisible(true);
+        this.setSize (300, 300);
+        
         this.add(this.panel);
+
+        this.setResizable(false);
+        this.setVisible(true);
     } 
 }
